623931ce004027fab21e48bf1db61a14e1103af0
Long Tran <LTran@HAEAus.com>
2025-03-27 21:56:35 -0700

    Optimize code, add lombok

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,7 +28,7 @@ public class CategoryController {
     }
 
     @PostMapping("/public/categories")
-    public ResponseEntity<String> addCategory(@RequestBody Category category) {
+    public ResponseEntity<String> addCategory(@Valid @RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Added category successfully", HttpStatus.CREATED);
     }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -8,51 +10,48 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.SynchronousQueue;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private List<Category> categories = new ArrayList<>();
+//    private List<Category> categories = new ArrayList<>();
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        categories.add(category);
+        categoryRepository.save(category);
         System.out.println("Category added successfully");
     }
 
     @Override
     public String deleteCategory(Long id) {
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
-        if (category != null) {
-            categories.remove(category);
-            return "Category deleted successfully";
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Category with id %s not found", id)));
+        categoryRepository.delete(category);
+        return "Category with categoryId " + id + " deleted successfully";
 
-        } else {
-            return "Category not found";
-        }
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
 
-        Optional<Category> categoryOptional = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst();
+        Category categoryUpdated = categoryRepository.findById(categoryId).
+            orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
-        if (categoryOptional.isPresent()) {
-            Category categoryToUpdate = categoryOptional.get();
-            categoryToUpdate.setCategoryName(category.getCategoryName());
-            return categoryToUpdate;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+//        Optional<Category> categoryOptional = categories.stream()
+//                .filter(c -> c.getCategoryId().equals(categoryId))
+//                .findFirst();
+
+        categoryUpdated.setCategoryName(category.getCategoryName());
+        categoryRepository.save(categoryUpdated);
+        return categoryUpdated;
     }
 }
